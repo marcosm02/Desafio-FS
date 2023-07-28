24b7eb5b87076a95db3fdfbab1852ffb12935c24
Marcos Machado <mmarcosvmachado@gmail.com>
2023-07-28 17:51:29 -0300

    add models

---
+++ backend/src/models/products.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../sequelize");
+
+const Products = sequelize.define("products", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Products;
