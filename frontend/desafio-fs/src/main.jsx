a43e08e96e8bb2fb2facba428e0d12878b426761
Marcos Machado <mmarcosvmachado@gmail.com>
2023-07-28 21:49:51 -0300

    add config, styles

--- frontend/desafio-fs/src/main.jsx
+++ frontend/desafio-fs/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { GlobalStyle } from "./styles/global";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyle />
+    <ToastContainer theme={"dark"} autoClose={1500} />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
